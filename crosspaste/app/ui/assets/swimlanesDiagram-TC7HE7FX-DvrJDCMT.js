f689f9cd16f9450cb51ed145ac4c0de883456831
YUNXI <2065234360@qq.com>
2026-09-15 18:24:49 +0800

    chore: 发布 v0.1.4 应用包（含前端产物与双架构二进制）

---
+++ crosspaste/app/ui/assets/swimlanesDiagram-TC7HE7FX-DvrJDCMT.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-KWPJA3E3-AecaROVM.js";import{_ as o}from"./mermaid.core-BWniEIWR.js";import"./chunk-5VM5RSS4-DL6JqwYY.js";import"./chunk-XXDRQBXY-CVgTikg0.js";import"./chunk-WEXAMYUT-D8jj0vaJ.js";import"./chunk-GWA4HPMP-DFjnB4P9.js";import"./MarkdownView-BFdKumDQ.js";import"./index-DoG7w7o9.js";import"./channel-CsMmUAwG.js";var a=o(t=>`${e(t)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,y=r({styles:m});export{y as diagram};
